1126ce8226fe52a4ba1f28fdac01ee5b12446feb
Victoria-Ogega <victoriaogega@gmail.com>
2025-04-29 12:03:20 +0300

    Added new changes

--- index.js
+++ index.js
@@ -24,44 +24,48 @@ console.log(availableSortedProducts(products))
 
 //  Create an object called grades where the keys are student names and the values are arrays of their scores. Write a function that calculates and prints each student's average score, and if the average is above 70, print "Pass"; otherwise, print "Fail" next to their name. (5 pts)
 
-// let grades = [
-//     {Shirley:[85,56,72]},
-//     {David:[55,50,42]},
-//     {Shelly:[88,46,42]},
-// ]
+let grades = {
+    Shirley:[85,56,72],
+    David:[55,50,42],
+    Shelly:[88,46,42],
+}
 
 
-// function calculateAverageScore(grades){
-//     let sum = 0
-//  for(let i =0;i< grades.length;i++)
-//         sum += grades.values i
-//  return  sum/grades i.length
+function calculateAverageScore(grades){
+    let sum = 0
+ for(let i in grades = 0);i in grades< grades.length;i in grades ++
+   sum += grades[i]    
+   let average = sum/grades[i].length
    
-// }
+}
 
-// console.log(calculateAverageScore(grades))
+console.log(calculateAverageScore(grades))
 
 
 
 
 
 //  Write a function constructor called User that takes username, email, and isActive (boolean). Create an array of users. Write a program that loops through the array and deactivates users who have not logged in recently (simulate this with a random isActive: false assignment) and print out the usernames of active users. (5 pts)
-function user(userDetails){
- return userDetails.map(item => {
-    if (item.isActive){
-        return item.name
-    } 
-    else{return "Account deactivated"}
-})
-
+function User(username,email,isActive){
+ this.username= username,
+ this.email = email,
+ this.isActive = isActive
 }
 
 let userDetails = [
-{name:'Victoria',email:'victoriaogega@gmail.com',isActive:true},
-{name:'Derrick',email:'derrickmambo@gmail.com',isActive:true},
-{name:'Ogega',email:'ogegabosiborori@gmail.com',isActive:false},
+new User('Victoria','victoriaogega@gmail.com',true),
+new User('Derrick','derrickmambo@gmail.com',true),
+new User('Ogega','ogegabosiborori@gmail.com',false),
 ]
-console.log(user(userDetails))
+
+  return  userDetails.forEach(user => {
+    if (user.isActive){
+        console.log(`${user.name}`) 
+    }
+    else{console.log("Account deactivated") }
+})
+
+
 
 
 
@@ -70,19 +74,30 @@ console.log(user(userDetails))
 
 //  You have an array of destination objects, each with name, distance (in km), and budgetRequired (in dollars). Write a function that accepts a maximum distance and a budget and returns all destinations the user can afford and reach within that distance. If none are found, return "No destinations available under your budget and distance". (5 pts)
 
-function destination(destinationDetails){
-return destinationDetails.filter(item=> {
-   item.distance <= '50km' && item.budgetRequired <= '100$'  
-    return destinationDetails.name
-   }
-)
-}
 let destinationDetails = [
-    {name:'Kisumu',distance:'100km',budgetRequired:'15$'},
-    {name:'United states',distance:'50km',budgetRequired:'155$'},
-    {name:'United Arab emirates',distance:'12km',budgetRequired:'125$'},
+    {name:'Kisumu',distance:10,budgetRequired:15},
+    {name:'United states',distance:500,budgetRequired:155},
+    {name:'United Arab emirates',distance:102,budgetRequired:125},
 ]
-console.log(destination(destinationDetails))
+
+function destination(destinationDetails,maximumDistance,budget){
+  let foundDestination =  destinationDetails.filter(item=> {
+   item.distance <= maximumDistance && item.budgetRequired <= budget  
+   }
+);
+}
+
+if(foundDestination.length > 0){
+    return foundDestination
+}
+else{return "No destination found"}
+
+
+
+
+console.log (destination(destinationDetails,100,155))
+
+// console.log(destination(destinationDetails))
 // Create a function constructor called Employee that accepts a name, position, and salary. Create an array of 5 employees. Write a program that increases the salary by 10% for employees who have the position "developer" and print the updated employee list. (5 pts)
 
 function Employee(name,position,salary){
@@ -99,9 +114,9 @@ let employees = [
   new Employee ('James','developer',205000)
 ]
 
- return employees.forEach (employee => {
+ employees.forEach (employee => {
     if (employee.position === 'developer'){
-        employee.salary *= 1.1 
-    }
-    console.log(employees)
+        employee.salary *= 1.1
+    }    
 })
+console.log(employees)
